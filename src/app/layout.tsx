ea5200edaf478905c22e5af082cd79f64d73d683
jeevandza <jeevandza4@gmail.com>
2025-08-10 14:03:21 +0530

    update png image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import type { Metadata } from "next";
 
-export default function RootLayout({ 
-  children 
-}: { 
-  children: React.ReactNode 
+export const metadata: Metadata = {
+  title: "Mahalasa",
+  description:
+    " Discover the network of Mahalasa temples across India and around the world, each preserving the divine traditions and serving devoted communities..",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
